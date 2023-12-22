17430c6631c309772042e662309e7f83f451982a
James Diaz Lopez <jmclaren@MBP-de-James.bbrouter>
2023-12-21 20:37:19 -0500

    [imp] 1.1 ✅

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,140 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+// import Swiper from 'swiper';
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+import { SwiperOptions } from 'swiper/types';
+import { Navigation, Pagination } from 'swiper/modules';
+
+// register Swiper custom elements
+register();
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements AfterViewInit {
+  swiperParams: SwiperOptions = {
+    slidesPerView: 2,
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    on: {
+      // init() {
+      //   console.log('swiper initialized');
+      // },
+    },
+  };
+
+  params = {
+    modules: [Navigation, Pagination],
+
+    // array with CSS styles
+    injectStyles: [
+      `
+      :host(.red) .swiper-wrapper {
+        background-color: red;
+      }
+      `,
+    ],
+
+    // array with CSS urls
+    injectStylesUrls: ['path/to/one.css', 'path/to/two.css'],
+  };
+
+
+  @ViewChild('swiper', { static: true }) swiperEl!: ElementRef | any;
 
   constructor() {}
 
+  onTest() {
+
+    // and now initialize it
+    this.swiperEl.nativeElement.initialize(); //* This code works well ✅
+
+
+    //set all parameters to Swiper element
+
+    // now we need to assign all parameters to Swiper element
+    // Object.assign(this.swiperEl, this.swiperParams); //! this code doesn't work well ❌
+    // this.swiperEl.nativeElement.slidesPerView = 3; // ✅✅✅
+
+    this.swiperEl.nativeElement.passedParams = this.swiperParams; //✅
+    this.swiperEl.nativeElement.swiperParams = this.swiperParams; //✅
+    this.swiperEl.nativeElement.config = this.swiperParams; //✅
+
+
+    console.log('ionViewDidEnter', this.swiperEl.nativeElement.swiper);
+    console.log(this.swiperEl);
+    console.log(this.swiperEl.nativeElement);
+
+    this.swiperEl.nativeElement.swiper.params = {
+      ...this.swiperEl.nativeElement.swiper.params,
+      ...this.swiperParams,
+    };
+    console.log("🚀 ~ file: tab1.page.ts:60 ~ Tab1Page ~ ionViewDidEnter ~ this.swiperEl.nativeElement.swiper.params:", this.swiperEl.nativeElement.swiper.params)
+
+    // and now initialize and update it
+    this.swiperEl.nativeElement.swiper.update();
+  }
+
+  ionViewDidEnter() {
+
+    // and now initialize it
+    this.swiperEl.nativeElement.initialize(); //* This code works well ✅
+
+
+    //set all parameters to Swiper element
+
+    // now we need to assign all parameters to Swiper element
+    // Object.assign(this.swiperEl, this.swiperParams); //! this code doesn't work well ❌
+    // this.swiperEl.nativeElement.slidesPerView = 3; // ✅✅✅
+
+    this.swiperEl.nativeElement.passedParams = this.swiperParams; //✅
+    this.swiperEl.nativeElement.swiperParams = this.swiperParams; //✅
+    this.swiperEl.nativeElement.config = this.swiperParams; //✅
+
+
+    console.log('ionViewDidEnter', this.swiperEl.nativeElement.swiper);
+    console.log(this.swiperEl);
+    console.log(this.swiperEl.nativeElement);
+
+    this.swiperEl.nativeElement.swiper.params = {
+      ...this.swiperEl.nativeElement.swiper.params,
+      ...this.swiperParams,
+    };
+    console.log("🚀 ~ file: tab1.page.ts:60 ~ Tab1Page ~ ionViewDidEnter ~ this.swiperEl.nativeElement.swiper.params:", this.swiperEl.nativeElement.swiper.params)
+
+    // and now initialize and update it
+    this.swiperEl.nativeElement.swiper.update();
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+    // this.swiper?.update(); // Use optional chaining
+  }
+
+  onUpdate() {
+    console.log('onUpdate');
+    // this.swiperEl.nativeElement?.update(); //! this code doesn't work well ❌
+
+
+    // if it was initialized with attributes
+    // this.swiperEl.nativeElement.setAttribute('slides-per-view', '3'); // ✅
+
+    // or if it was initialized with props
+    this.swiperEl.nativeElement.slidesPerView = 3; // ✅
+  }
+
+  onSlideNext() { // ✅ SUCCESS
+    console.log('onSlideNext');
+    this.swiperEl.nativeElement.swiper.slideNext();
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements AfterViewInit {
+  @ViewChild('swiperElement', { static: false }) swiperElement!: ElementRef;
 
-  constructor() {}
+  swiper!: Swiper;
 
+  swiperParams: SwiperOptions = {
+    speed: 400,
+    spaceBetween: 100,
+    autoplay: {
+      delay: 1000,
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  }
+
+
+  ngAfterViewInit() {
+    this.swiper = new Swiper(this.swiperElement.nativeElement, this.swiperParams);
+  }
+
+  slideNext() {
+    this.swiper.slideNext();
+  }
+
+  slidePrev() {
+    this.swiper.slidePrev();
+  }
+
+  onUpdate() {
+    this.swiper.params.slidesPerView = 3;
+    this.swiper.update(); // Success to update
+    console.log('onUpdate');
+  }
 }
